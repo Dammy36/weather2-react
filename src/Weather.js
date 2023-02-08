4640dec72ed83cd993cc2f1fc359e21276a2a8d8
Dammy36 <113221009+Dammy36@users.noreply.github.com>
2023-02-08 14:06:15 +0100

    changed the  Git repo Url

--- src/Weather.js
+++ src/Weather.js
@@ -5,10 +5,10 @@ import './App.css';
 export default function Weather() {  
 
     return (
-      <small className="hosted">
+      <small className="hosted">This project was created by Oseyemi Dammy and is
         <a href="https://github.com/Dammy36/weather2-react"  rel="noreferrer" target="_blank">
-          Opensorce code </a>
-        by Oseyemi Dammy {}       
+          Opensourced code on Github </a> and
+         {""}       
         <a href="https://inspiring-stroopwafel-6ba231.netlify.app/" rel="noreferrer" target="_blank"> 
            Hosted on Netlify
         </a>
